39cc3da733db4f4f1cb6cb5e6b05a4ff353da7e5
fazeenamin <114429696+fazeenamin@users.noreply.github.com>
2024-05-01 12:08:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const responsiveBtn = document.querySelector(".resp-btn");
+const respSearch = document.querySelector("resp");
+
+responsiveBtn.addEventListener("click", function () {
+  responsiveBtn.style.display = none;
+  respSearch.style.display = inline;
+  const html =
+    ' <input class="Search-box resp" type="text" placeholder="Search" />';
+  document.insert;
+});
